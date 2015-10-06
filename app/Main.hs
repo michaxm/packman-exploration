2fedb15910a2471bda7f5eb090bfd3d5f073852c
Axel Mannhardt <notset@example.com>
2015-10-06 10:50:09 +0200

    initial

---
+++ app/Main.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Data.Char (isLower)
+import GHC.Packing
+
+import Lib
+
+main = do
+
+  putStrLn "testing (un-)wrapping in Serializable:"
+  blob <- trySerialize f 
+  print blob
+  deserialize blob >>= apply
+
+  putStrLn "testing complete binary serialization:"
+  bs <- wrapToBinary f
+  print bs
+  unwrapFromBinary bs >>= apply
+
+  putStrLn "I did not really bother to make serialization with read/show work, deserialization will fail:"
+  str <- wrapToString f
+  print str
+  unwrapFromString str >>= apply
+  where
+    -- serialization and deserialization both need monomorphism, tried an example with an inferred function type here
+    f = isLower
+    apply f = putStrLn $ "applying: " ++ (toStr $ map f "AaA") ++ "\n\n"
+      where
+        -- ... show needs monomorphism, too ..
+        toStr :: [Bool] -> String
+        toStr = show
---
+++ src/Lib.hs
@@ -0,0 +1,19 @@
+module Lib where
+
+import Data.Binary
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import Data.Typeable (Typeable)
+import GHC.Packing
+
+wrapToBinary :: (Typeable a) => a -> IO L.ByteString
+wrapToBinary a = trySerialize a >>= return . encode
+  
+unwrapFromBinary :: (Typeable a) => L.ByteString -> IO a
+unwrapFromBinary = deserialize . decode
+
+wrapToString :: (Typeable a) => a -> IO String
+wrapToString a = trySerialize a >>= return . show
+
+unwrapFromString :: (Typeable a) => String -> IO a
+unwrapFromString = deserialize . read
